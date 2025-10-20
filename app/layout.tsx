fec3b5f459ce025aa4cc9e12661acf5d1a993e2d
mohdarif23 <mohammedarif2303@gmail.com>
2025-10-20 21:03:41 +0530

    updation

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="LWo4AHpMtilr-8ORZJWnVWzOHGS4e2EJckFzAk_hl6g" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
